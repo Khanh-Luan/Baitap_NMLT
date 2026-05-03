9cfe6c42996c5518c7d5bebe33894d4b0c95c672
Khanh-Luan <luankeng2007@gmail.com>
2026-05-03 14:27:09 +0700

    Add files via upload

---
+++ Baitap_HocDemTrongMang/HocDemTrongMang.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+int main () {
+    int n, x;
+    cin >> n >> x;
+
+    vector<int> a(n);
+
+    int xuathien = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (a[i] == x) xuathien++;
+    }
+    cout << xuathien;
+}
